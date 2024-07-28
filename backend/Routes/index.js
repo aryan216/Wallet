456c5bb906273ebede430cdc9f4e843ab6b67efe
aryan216 <aniketani603@gmail.com>
2024-07-28 23:47:01 +0530

    completed backend

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -1,5 +1,7 @@
 const express=require("express");
-const userRouter=require("./Routes/user")
+const userRouter=require("./Routes/user");
+const accountRouter=require("./Routes/account")
 const router=express.Router();
 router.use("/user",userRouter)
+router.use("account",accountRouter)
 module.exports=router;
