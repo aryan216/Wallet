6ad0f2fcef68405a8202c84d9ae0d449a7a5e161
aryan216 <aniketani603@gmail.com>
2024-08-03 18:16:09 +0530

    frontend boilerplate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
+import { Signup } from "./pages/Signup"
+import { Signin } from "./pages/Signin";
+import { Transfer } from "./pages/Transfer";
+import { Dashboard } from "./pages/Dashboard";
+
+
 
 function App() {
 
   return (
     <div>
-        abcd
+        <BrowserRouter>
+          <Routes>
+            <Route path="/Signin" element={<Signin/>}/>
+            <Route path="/Signup" element={<Signup/>}/>
+            <Route path="/Dashboard" element={<Dashboard/>}/>
+            <Route path="/Transfer" element={<Transfer/>}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,7 @@
+export function Signup(){
+    return(
+        <div className="bg-slate-600">
+
+        </div>
+    )
+}
