d90da2e70c1a363aee266bb13adbae67eb36381d
aryan216 <aniketani603@gmail.com>
2024-08-04 23:50:54 +0530

    lazy stuff

---
+++ frontend/src/Components/Heading.jsx
@@ -0,0 +1,9 @@
+export function Heading({label}){
+    return(
+        <div className="text-4xl font-sans font-semibold">
+            {label}
+        </div>
+        
+    )
+
+}
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,12 @@
+import { Heading } from "../Components/Heading"
+
+
 export function Signup(){
     return(
-        <div className="bg-slate-600 h-screen flex justify-center ">
-              aniket
+        <div className="bg-slate-600 h-screen flex justify-center items-center ">
+           <div className="bg-white h-96 rounded-lg w-72 flex justify-center p-3">
+                <Heading label={"Sign Up"}/>
+           </div>
         </div>
     )
 }
