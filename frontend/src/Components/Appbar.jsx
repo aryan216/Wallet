6a3355a3cdd2f59a94559fed01655cc14975709e
aryan216 <aniketani603@gmail.com>
2024-08-15 00:15:36 +0530

    navbar

---
+++ frontend/src/Components/Appbar.jsx
@@ -0,0 +1,19 @@
+export function Appbar(){
+    return(
+        <div className="shadow h-14 flex justify-between">
+            <div className="flex items-center m-2 p-2 text-lg">
+                Paytm 
+            </div>
+            <div className="flex items-center m-4 p-2 ">
+                <div className="m-4 text-xl">
+                    Hello
+                </div>
+                <div className="rounded-full h-12 w-12 bg-slate-400 flex justify-center mt-2 mb-2">
+                    <div className="flex flex-col justify-center h-full text-xl">
+                        U
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,12 @@
+import { Appbar } from "../Components/Appbar";
+import {Balance } from "../Components/Balance"
 export function Dashboard(){
     return(
         <div>
-            
+            <Appbar/>
+            <div className="m-8">
+                <Balance value={10000}/>
+            </div>
         </div>
     )
 }
